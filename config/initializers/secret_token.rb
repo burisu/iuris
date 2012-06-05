06507ce3592c9c1d55964b7819fcf012abf96224
Brice Texier <burisu@oneiros.fr>
2012-06-05 20:04:36 +0200

    Adds empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iuris::Application.config.secret_token = 'a1f5cbe95911a3fab6673ad2611f2801652721ac04d60566040c40ef919f80dea1f2924fb9829c25060cc5fe928b22481d3925928f59fbfbd130072cc5651e17'
